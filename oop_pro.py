3d03700c1ca554d5c1a335e0f27d9113080175ad
dataengineer4912 <tejaspawar8536@gmail.com>
2025-12-10 17:42:27 +0530

    initial commit

---
+++ oop_pro.py
@@ -0,0 +1,33 @@
+class NLPApp:
+    def __init__(Self):
+        Self.__database={}
+        Self.__first_menu()
+
+    def __first_menu(self):
+        first_input=input("""
+                        Hi! How would you like to proced?
+                          1. Not a member? Register
+                          2. Alreday Member? Login
+                          3. Exist
+                          """)
+        
+        if first_input == '1':
+            self.__register()
+
+        elif first_input == '2':
+            self.__login()
+
+        else:
+            exit()
+
+    
+
+    def __register(self):
+        print("register")
+        pass
+
+    def __login(self):
+        print("login")  
+        pass  
+
+obj = NLPApp()   
